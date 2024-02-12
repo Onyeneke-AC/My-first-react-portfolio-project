bed86ea5ac50ab1d8a48fc1530089327b658e47a
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-12 23:24:10 +0100

    Propriety product section finalization

--- src/components/templates/body/PropProd.js
+++ src/components/templates/body/PropProd.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { Button } from '../../../atoms/Button';
 import './PropOem.css';
 import PropOemSliders from '../../../organisms/PropOemSliders';
+import { PropProdData } from '../../../assets/data/PropProdData';
 
 function PropProd() {
   return (
     <div className='prop-oem'>
       <h2 className="sub_h2">
-        OEM products
+        Propiety products
       </h2>
       <p className='info-p'>
         Solutions Developed by Tenece
       </p>
-      <PropOemSliders />
+      <PropOemSliders dataItem = {PropProdData}/>
       <div className="btn_int">
             <Button className='btns' buttonStyle='btn--primary' buttonRoute='/clients' buttonSize='btn--normal'>
                     Read more
--- src/organisms/PropOemSliders.js
+++ src/organisms/PropOemSliders.js
@@ -1,27 +1,28 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, EffectFade, Autoplay } from 'swiper';
+import { Navigation, EffectFade, Autoplay, Pagination } from 'swiper';
 import 'swiper/css'
 import 'swiper/css/autoplay'
+import 'swiper/css/pagination'
 // import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
 import './PropOemSliders.css';
-import { PropProdData } from '../assets/data/PropProdData';
+// import { PropProdData } from '../assets/data/PropProdData';
 import { useRef } from 'react';
 
-function PropOemSliders() {
+function PropOemSliders({dataItem = []}) {
 
   const swiperNavPrevRef = useRef();
   const swiperNavNextRef = useRef();
 
   return (
     <Swiper
-      modules={[Navigation, EffectFade, Autoplay]}
+      modules={[Navigation, EffectFade, Autoplay, Pagination]}
       navigation = {{
         prevEl: swiperNavPrevRef,
         nextEl: swiperNavNextRef
       }}
-      autoplay
+      pagination={{clickable: true}}
       effect={'slide'}
       spaceBetween={70}
       slidesPerView={4}
@@ -29,15 +30,18 @@ function PropOemSliders() {
       speed={800}
       loop={"true"}
       className='swipe'
-
+      id='swipe'
+      
       onInit={(swiper) => {
         swiper.params.navigation.prevEl = swiperNavPrevRef.current;
         swiper.params.navigation.nextEl = swiperNavNextRef.current;
         swiper.navigation.init();
         swiper.navigation.update();
       }}
-    >
-      {PropProdData.map( item => 
+      
+      autoplay={{ delay: 10000 }}
+      >
+      {dataItem.map( item => 
       <SwiperSlide className="swipe-slide">
         <figure className='fig'>
         <img src={item.src} alt={item.alt} className='swipe-img'/>
