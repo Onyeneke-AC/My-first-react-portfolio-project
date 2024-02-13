9af77ddbee243b4a424360cd6620bd37f8b1a835
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-13 22:17:45 +0100

    swiper slide setup for services section

--- src/components/templates/body/Services.js
+++ src/components/templates/body/Services.js
@@ -9,43 +9,37 @@ function Services() {
             Our Services
         </h2>
         <Swiper spaceBetween={20}  speed={800}  slidesPerView={1}  className='swiper-service'>
-            <SwiperSlide className="swiper-slide">
-                <div className="serv1">
-                    <div className="serv">    
-                        <i className="fas fa-caret-up" />
-                         <h3 className="serv__h3">
-                            Advisory
-                        </h3>
-                        <p>
-                            We help deliver better-quality business performance by addressing the IT and business agendas together; an effective, well-managed IT system is one of the most valuable business advantages an organization can secure.
-                        </p>
-                    </div>
+            <SwiperSlide className="swipe-me serv1">
+                <div className="serv">    
+                    <i className="fas fa-caret-up" />
+                     <h3 className="serv__h3">
+                        Advisory
+                    </h3>
+                    <p>
+                        We help deliver better-quality business performance by addressing the IT and business agendas together; an effective, well-managed IT system is one of the most valuable business advantages an organization can secure.
+                    </p>
                 </div>
             </SwiperSlide>
-            <SwiperSlide className="swiper-slide">
-                <div className="serv2">
-                    <div className="serv">
-                        <i className="fas fa-caret-up" />
-                        <h3 className="serv__h3">
-                            Information Technology
-                        </h3>
-                        <p>
-                            Our managed services are ideal if you don't have what you need in-house for day-to-day management, we do the work and you get the valuable information you need for your organization.
-                        </p>
-                    </div>
+            <SwiperSlide className="swipe-me serv2">
+                <div className="serv">
+                    <i className="fas fa-caret-up" />
+                    <h3 className="serv__h3">
+                        Information Technology
+                    </h3>
+                    <p>
+                        Our managed services are ideal if you don't have what you need in-house for day-to-day management, we do the work and you get the valuable information you need for your organization.
+                    </p>
                 </div>
             </SwiperSlide>
-            <SwiperSlide className="swiper-slide">
-                <div className="serv2">
-                    <div className="serv">
-                        <i className="fas fa-caret-up" />
-                        <h3 className="serv__h3">
-                            Managed Services
-                        </h3>
-                        <p>
-                            We improve your business for innovation and growth while driving bottom-line benefits to drive growth, eliminate downtime, scale operations and improve application availability with the procedure management of IT resources.
-                        </p>
-                    </div>
+            <SwiperSlide className="swipe-me serv3">
+                <div className="serv">
+                    <i className="fas fa-caret-up" />
+                    <h3 className="serv__h3">
+                        Managed Services
+                    </h3>
+                    <p>
+                        We improve your business for innovation and growth while driving bottom-line benefits to drive growth, eliminate downtime, scale operations and improve application availability with the procedure management of IT resources.
+                    </p>
                 </div>
             </SwiperSlide>
         </Swiper>
