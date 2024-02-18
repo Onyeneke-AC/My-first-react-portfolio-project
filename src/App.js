54658ccf7c8c4d510e22449d8b3823dd570a970d
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-18 22:29:30 +0100

    Partner page initialization

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes as Switch , Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Products  from './components/pages/Partners';
+import Partners from './components/pages/Partners';
 
 function App() {
 
@@ -10,6 +12,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/" exact Component={Home}/>
+          <Route path='/partners' Component={Partners} />
         </Switch>
       </Router>
     </>
---
+++ src/components/pages/Partners.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Sidebar from '../templates/header/Sidebar'
+import News from '../templates/header/News'
+import Footer from '../templates/footer/Footer'
+import Navbar from '../templates/header/Navbar'
+
+
+function Partners() {
+  return (
+    <>
+    <Navbar />
+    <Sidebar />
+    <News />
+    <Footer />
+    </>
+  )
+}
+
+export default Partners
