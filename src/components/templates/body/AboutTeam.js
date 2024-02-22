043aabb0322aa2719a89f253ad7218baaa631ce5
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-02-22 23:54:09 +0100

    looping through the team lead images

--- src/components/templates/body/AboutTeam.js
+++ src/components/templates/body/AboutTeam.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './AboutUs.css'
+import './AboutUs.css';
+import { TeamData } from '../../../assets/data/TeamData';
 
 
 function AboutTeam() {
@@ -9,6 +10,20 @@ function AboutTeam() {
             <h1 className="our">Our</h1>
             <h1> Leadership</h1>
             <h1 className="team">Team</h1>
+        </div>
+        <div className="card-container">
+            
+            {
+                TeamData.map((member, index) => 
+                    (
+                    <>
+                        <figure  key={index} className="card">
+                        <img src={member.src} alt="" />
+                        </figure>
+                    </>
+                    )
+                )
+            }
         </div>
         
     </div>
